bbf88c040964f2ac4a04ba1f2559dc142ecca8f6
Zakaria Ridouh <zridouh@live.com>
2016-05-30 13:55:18 -0700

    main.js: fix a small bug

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function game() {
-'use strict';
+    'use strict';
 
-  var name = prompt("What is your name?");
+    var name = prompt("What is your name?");
 
-  function checkName(){
-    if (name === "" || name === null) {
-      alert("Please give a valid name!");
-      name = prompt("What is your name?");
-      checkName();
+    function checkName(){
+      if (name === "" || name === null) {
+        alert("Please give a valid name!");
+        name = prompt("What is your name?");
+        checkName();
+      }
     }
-  }
   // Check the entered name
   checkName();
 
@@ -19,9 +19,9 @@ function game() {
 
   var myChoice = prompt("So, do you like scary stories, " + name + "?");
 
-  if (myChoice.toLowerCase == "yes") {
+  if (myChoice.toLowerCase === "yes") {
       alert("So we depart on our journey...");
-  } else if (myChoice.toLowerCase() == "no") {
+  } else if (myChoice.toLowerCase() === "no") {
       alert("This is gonna be FUN! Haha!");
   } else {
       checkMyChoice();
@@ -30,7 +30,6 @@ function game() {
   function checkMyChoice(){
     if (myChoice.toLowerCase !== "yes" || myChoice.toLowerCase !== "no"){
       alert("Please answer 'yes' or 'no'");
-      myChoice = prompt("So, do you like scary stories, " + name + "?");
       checkMyChoice();
     }
   }
@@ -46,11 +45,11 @@ function game() {
       "You're at home, alone, watching the TV and hear a knock on the door. You hear more banging on the door. Your heatbeat speeds up and you're very scared. What do you do? \nA) Ignore the knocking nervously. \nB) Start screaming your head off. \nC) Couragously go and open the door. \nPlease choose A, B, or C "
   );
 
-  if (alone.toLowerCase() == "c") {
+  if (alone.toLowerCase() === "c") {
       alert( name + " , WOW! You are very couragous! Sadly your bravery comes to an end when you find that it was just your friends pranking you. You guys go take a walk.");
-  } else if (alone.toLowerCase() == "b") {
+  } else if (alone.toLowerCase() === "b") {
       alert( name + " , too bad because they was your friends pulling a prank on you. You actually fell for it! You guys go take a walk.");
-  } else if (alone.toLowerCase() == "a") {
+  } else if (alone.toLowerCase() === "a") {
       alert( name + " , too bad because it was your friends just pulling a prank on you. You actually fell for it! You guys decide take a walk.");
   } else {
     checkAlone();
@@ -61,18 +60,17 @@ function game() {
   );
 
   function checkRumors(){
-    if(rumors.toLowerCase() == "a" || rumors.toLowerCase() == "b" || rumors.toLowerCase() == "c"){
+    if (rumors.toLowerCase() !== "a" || rumors.toLowerCase() !== "b" || rumors.toLowerCase() !== "c") {
       alert("Please respond with 'a', 'b', or 'c'!");
       checkRumors();
     }
   }
 
-  if (rumors.toLowerCase() == "b") {
+  if (rumors.toLowerCase() === "b") {
       alert("Boy! You have the guts!");
-  } else if(rumors.toLowerCase() == "a" || rumors.toLowerCase() == "c"){
+  } else if(rumors.toLowerCase() === "a" || rumors.toLowerCase() === "c"){
       alert("Your cowardliness doesn't save you! He gets you! Sorry!");
       done();
-      return;
   } else {
     checkRumors();
   }
@@ -88,9 +86,9 @@ function game() {
       "You run home sprint up the stairs, jump under the bed and hide. You hear 'Chain-Saw Billy' capture your friend. What do you do? \nA) Get tired of all this stuff and suicide out the window. \nB) Choke yourself. \nC) Try to protect yourself with anything you can find."
   );
 
-  if (runafter.toLowerCase() == "c") {
+  if (runafter.toLowerCase() === "c") {
       alert("Sorry, you get killed in inexplicable ways");
-  } else if (runafter.toLowerCase() == "a" || runafter.toLowerCase() == "b"){
+  } else if (runafter.toLowerCase() === "a" || runafter.toLowerCase() === "b"){
       alert("You'd kill yourself?! Wow must be depressed! Please call the local Suicide Hotline!");
   } else {
     checkRunAfter();
