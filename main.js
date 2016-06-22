0a511f83f681d35244ed2d2c9256f51b593fa717
Zakaria Ridouh <zridouh@live.com>
2016-06-22 02:59:26 -0700

    main: normalize user input some more

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function game() {
     'use strict';
 
-    var name = prompt("What is your name?");
+    var name = prompt("What is your name?").toLowerCase().trim();
 
     function checkName(){
       if (name === "" || name === null) {
         alert("Please give a valid name!");
-        name = prompt("What is your name?");
+        name = prompt("What is your name?").toLowerCase().trim();
         checkName();
       }
     }
@@ -21,15 +21,15 @@ function game() {
   function checkMyChoice() {
     if (myChoice.toLowerCase != "yes" || myChoice.toLowerCase != "no") {
       alert("Please answer 'yes' or 'no'");
-      myChoice = prompt("So, do you like scary stories, " + name + "?");
+      myChoice = prompt("So, do you like scary stories, " + name + "?").toLowerCase().trim();
     }
   }
 
-  var myChoice = prompt("So, do you like scary stories, " + name + "?");
+  var myChoice = prompt("So, do you like scary stories, " + name + "?").toLowerCase().trim();
 
-  if (myChoice.toLowerCase === "yes") {
+  if (myChoice === "yes") {
       alert("So we depart on our journey...");
-  } else if (myChoice.toLowerCase() === "no") {
+  } else if (myChoice === "no") {
       alert("This is gonna be FUN! Haha!");
   } else {
       checkMyChoice();
@@ -45,22 +45,22 @@ function game() {
 
   var alone = prompt(
       "You're at home, alone, watching the TV and hear a knock on the door. You hear more banging on the door. Your heatbeat speeds up and you're very scared. What do you do? \nA) Ignore the knocking nervously. \nB) Start screaming your head off. \nC) Couragously go and open the door. \nPlease choose A, B, or C "
-  );
+  ).toLowerCase().trim();
 
-  if (alone.toLowerCase() === "c") {
+  if (alone === "c") {
       alert( name + " , WOW! You are very couragous! Sadly your bravery comes to an end when you find that it was just your friends pranking you. You guys go take a walk.");
-  } else if (alone.toLowerCase() === "b") {
+  } else if (alone === "b") {
       alert( name + " , too bad because they was your friends pulling a prank on you. You actually fell for it! You guys go take a walk.");
-  } else if (alone.toLowerCase() === "a") {
+  } else if (alone === "a") {
       alert( name + " , too bad because it was your friends just pulling a prank on you. You actually fell for it! You guys decide take a walk.");
-  } else if (alone.toLowerCase() === "" || alone === undefined) {
+  } else if (alone === "" || alone === undefined) {
     checkAlone();
   } else {
     checkAlone();
   }
 
   function checkRumors() {
-    if (rumors.toLowerCase() != "a" || rumors.toLowerCase() != "b" || rumors.toLowerCase() != "c") {
+    if (rumors != "a" || rumors != "b" || rumors != "c") {
       alert("Please respond with 'a', 'b', or 'c'!");
       rumors = prompt("Your friends tell you that rumors are circulating about 'Chain-Saw Billy' the new boogyman around town. He catches his victems and tortures them until death with his chainsaw. You and your buds hear a yell then a chainsaw starting up! \nYou look back and see 'Chain-Saw Billy' cutting off a mans head... He looks at you and comes sprinting after you. What do you do? \nA. Run away screaming your head off. \nB. Stand your ground and try to protect you and your friends. \nC. Go try to get help.");
     }
@@ -70,16 +70,16 @@ function game() {
       "Your friends tell you that rumors are circulating about 'Chain-Saw Billy' the new boogyman around town. He catches his victems and tortures them until death with his chainsaw. You and your buds hear a yell then a chainsaw starting up! \nYou look back and see 'Chain-Saw Billy' cutting off a mans head... He looks at you and comes sprinting after you. What do you do? \nA. Run away screaming your head off. \nB. Stand your ground and try to protect you and your friends. \nC. Go try to get help."
   );
 
-  if (rumors.toLowerCase() === "b") {
+  if (rumors === "b") {
       alert("Boy! You have the guts!");
-  } else if(rumors.toLowerCase() === "a" || rumors.toLowerCase() === "c"){
+  } else if(rumors === "a" || rumors === "c"){
       alert("Your cowardliness doesn't save you! He gets you! Sorry! \n \n BUT, you still manage to excape him!");
   } else {
     checkRumors();
   }
 
   function checkRunAfter() {
-    if (runAfter.toLowerCase() != "a" || runAfter.toLowerCase() != "b" || runAfter.toLowerCase() != "c") {
+    if (runAfter != "a" || runAfter != "b" || runAfter != "c") {
       alert("Please respond with 'a', 'b', or 'c'!");
       runAfter = prompt(
           "You run home sprint up the stairs, jump under the bed and hide. You hear 'Chain-Saw Billy' capture your friend. What do you do? \nA) Get tired of all this stuff and suicide out the window. \nB) Choke yourself. \nC) Try to protect yourself with anything you can find."
@@ -89,11 +89,11 @@ function game() {
 
   var runAfter = prompt(
       "You run home sprint up the stairs, jump under the bed and hide. You hear 'Chain-Saw Billy' capture your friend. What do you do? \nA) Get tired of all this stuff and suicide out the window. \nB) Choke yourself. \nC) Try to protect yourself with anything you can find."
-  );
+  ).toLowerCase().trim();
 
-  if (runAfter.toLowerCase() === "c") {
+  if (runAfter === "c") {
       alert("Sorry, you get killed in inexplicable ways");
-  } else if (runAfter.toLowerCase() === "a" || runAfter.toLowerCase() === "b"){
+  } else if (runAfter === "a" || runAfter === "b"){
       alert("You'd kill yourself?! Wow must be depressed! Please call the local Suicide Hotline!");
   } else {
     checkRunAfter();
